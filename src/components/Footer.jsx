7227f6482fa0af0ba2cc98435805f86236043042
unknown <nicoscs12@gmail.com>
2023-11-27 19:57:10 -0300

    frontend finalizado

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <div className="w-screen bg-sky-700 mt-10 p-5">
       <div className="flex justify-around">
-        <p className="font-bold text-white">Copyright 2023 - CLICK SHOPIFY </p>
+        <p className="font-bold text-white">© Copyright 2023 - ClickBOOK</p>
         <p className="font-bold text-white">
           {" "}
           Rivadavia 1430, CP 2177 ,Bigand , Santa Fe
